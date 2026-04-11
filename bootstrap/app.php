38599ccd429f8768c51d554ab96fb6a0b1cdd8c8
danieljimros <danieljimros@gmail.com>
2026-04-11 18:37:37 +0200

    Configuración para arreglar el bug de que no se mostraban los componentes React en el navegador

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,16 +11,20 @@ use Illuminate\Http\Request;
 use Illuminate\Validation\ValidationException;
 use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
-use Throwable;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
+        api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->encryptCookies(except: ['appearance', 'sidebar_state']);
+
+        $middleware->api(prepend: [
+            \Illuminate\Http\Middleware\HandleCors::class,
+        ]);
 
         $middleware->web(append: [
             HandleAppearance::class,
